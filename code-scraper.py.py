571a6ddd328cda79449400a1764c6b24aa06d6a7
zincodez <zinvscodez@gmail.com>
2026-05-13 12:07:19 +0530

    Delete code-scraper.py.py

--- code-scraper.py.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup as bs
-import requests
-url=r'https://quotes.toscrape.com/'
-data=requests.get(url).text
-soup=bs(data,'lxml')
-with open('quotes.txt','w') as fh:
-    for quotes in soup.find_all('div',class_='quote'):
-        fh.write(quotes.span.text+"\n")
-        fh.write("by-" +quotes.small.text + '\n')
